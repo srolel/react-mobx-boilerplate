074a14f9ddc63875d3c4f407e7883ef097fa1d01
Sharon Rolel <sharon.rolel@activeallocator.com>
2017-04-04 07:06:13 +0300

    Link component

--- src/components/Core.tsx
+++ src/components/Core.tsx
@@ -1,5 +1,6 @@
-import * as React from 'react';
+import * as   React from 'react';
 import * as styles from './styles.css';
+import Link from './Link';
 
 /**
  * <Core />
@@ -7,14 +8,6 @@ import * as styles from './styles.css';
  * This makes it simple to have a component at the index '/' route
  * of our application.
  */
-
-const pushState = (url: string) => (e: React.MouseEvent<any>) => {
-  e.preventDefault();
-  history.pushState(null, "", url);
-}
-
-const Link = ({ href, ...rest }: React.HTMLAttributes<any>) =>
-  <a href={href} onClick={pushState(href)} {...rest} />;
 
 const Core = ({ children }) =>
   <div>
---
+++ src/components/Link.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const pushState = (url: string) => (e: React.MouseEvent<any>) => {
+    e.preventDefault();
+    history.pushState(null, "", url);
+}
+
+const Link = ({ href, ...rest }: React.HTMLAttributes<any>) =>
+    <a href={href} onClick={pushState(href)} {...rest} />;
+
+export default Link;
