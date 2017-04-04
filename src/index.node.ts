ab240894df4c97369e58ccd90f3f59cde8cd0910
Sharon Rolel <sharon.rolel@activeallocator.com>
2017-04-04 22:03:50 +0300

    small fix

--- src/index.node.ts
+++ src/index.node.ts
@@ -1,14 +1,14 @@
 import * as React from 'react';
 import * as ReactDOMServer from 'react-dom/server';
 import Root from './root';
-import AppState from './AppState';
+import {AppStateProps} from './AppState';
 import App from './App';
 
 /*
 * Entry point for the node bundle, used for server-side rendering.
 */
 
-export default async (pathname = '/', appState?: AppState) => {
+export default async (pathname = '/', appState?: AppStateProps) => {
     const app = new App(appState);
     await app.updateLocation(pathname);
     return ReactDOMServer.renderToString(
