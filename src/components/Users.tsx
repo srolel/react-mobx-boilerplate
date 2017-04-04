6f38de3dc286978ed5e83e5c66394c0eed0bbc11
Sharon Rolel <sharon.rolel@activeallocator.com>
2017-04-04 07:06:38 +0300

    showcase routing

--- src/components/Users.tsx
+++ src/components/Users.tsx
@@ -1,5 +1,32 @@
 import * as React from 'react';
+import Routing from '../Routing';
+import { observer } from 'mobx-react';
+import Link from './Link';
 
-const Users = () => <div>Users</div>;
+@observer
+class Users extends React.Component<any, any> {
+
+    context: {
+        routing: Routing
+    }
+
+    static contextTypes = {
+        routing: React.PropTypes.any
+    };
+
+    render() {
+        return (
+            <div>
+                <div>Users</div>
+                <ul>
+                    <li><Link href="/users/1">1</Link></li>
+                    <li><Link href="/users/2">2</Link></li>
+                    <li><Link href="/users/3">3</Link></li>
+                </ul>
+                route params: <code>{JSON.stringify(this.context.routing.route.params, null, 4)}</code>
+            </div>
+        );
+    }
+}
 
 export default Users;
