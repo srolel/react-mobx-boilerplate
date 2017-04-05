b52039bdb2761f073e60acb4c95337c6a7a0373e
Benjamin Gruenbaum <inglor@gmail.com>
2017-04-05 16:01:55 +0300

    Update routes.tsx

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,14 +1,26 @@
+
+import * as React from 'react';
+import AppState from './stores/AppState';
 /*
     * webpack does static analysis over `System.import` calls to split the app code
     * into chunks. We must include each import explicitly.
     */
-import * as React from 'react';
-import AppState from './stores/AppState';
 
 export interface Route {
-    route: string;
-    onEnter?: (appState: AppState, params: object) => Promise<void>;
-    getComponent: (appState: AppState, params: object) => Promise<JSX.Element>;
+   // The actual route name, containing what URLs this route matches. 
+   // For example for defining a route for /books - you'd pass `books'.
+   // parameters can be passed with `:` express like syntax, for example `/books/:id/`
+   route: string;
+   // This is how you tell what route gets what component, the decision can be made asynchronously
+   // and data fetching can also occur here. Typically you'd initialize the data a page needs 
+   // to a consistent state here 
+   getComponent: (appState: AppState, params: object) => Promise<JSX.Element>; 
+   // This optionally (if passed) gets called after the routing happens. Server-Side-Rendering also 
+   // waits for this to finish before the route actually changes. 
+   // This is useful because routes typically need additional data loading logic _after_ they mount.
+   // For example - a books component might require data after loading (and a loading indicator can be
+   // shown in the meantime).
+   onEnter?: (appState: AppState, params: object) => Promise<void>;
 }
 
 let routes: Route[];
@@ -27,7 +39,7 @@ routes = [{
     route: '/users/?:id?',
     async onEnter(appState, params: { id?: string }) {
         appState.setMessage('');
-        if (params.id) {
+        if (params.id) { // simuate additional fetching that needs to happen after route loads
             appState.setMessage(`fetching data for user ${params.id}...`);
             await new Promise(r => setTimeout(r, 500));
             appState.setMessage(`data fetched for user ${params.id}`);
